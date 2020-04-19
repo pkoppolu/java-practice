497b0df4d5c2ba873d0ee6fb404b4bb3ff5e5406
Prudhvi K <Prudhvi K@192.168.0.18>
2020-04-19 17:57:00 -0500

    DefaultConstructor Message

---
+++ src/com/core/java/constructors/DefaultConstructor.java
@@ -0,0 +1,12 @@
+package com.core.java.constructors;
+
+public class DefaultConstructor {
+	DefaultConstructor() {
+		System.out.println("Default Constructor");
+	}
+
+	public static void main(String[] args) {
+		DefaultConstructor d1 = new DefaultConstructor();
+
+	}
+}
